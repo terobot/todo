d28850d4bcdba89647e1c56139dbdc972d6e0653
terobot <tero.t.kononen@gmail.com>
2021-11-28 00:04:24 +0200

    Add init dev projects

--- src/components/sidePanel.js
+++ src/components/sidePanel.js
@@ -1,7 +1,20 @@
+import Service from '../utils/services'
+
+const ProjectEL = (title) => {
+    const projectEl = document.createElement('div')
+    projectEl.innerHTML = `${title}`
+
+    return projectEl
+}
+
 const SidePanel = () => {
     const sidePanel = document.createElement('div')
+    const projects = document.createElement('div') 
     sidePanel.classList.add('sidePanel')
-    
+    projects.classList.add('projects')
+    Service.getProducts().map(x => projects.appendChild(ProjectEL(x.title)))
+    sidePanel.appendChild(projects)
+
     return sidePanel
 }
 
--- src/utils/services.js
+++ src/utils/services.js
@@ -1,6 +1,19 @@
+import Schema from './schema'
+
+//init product for dev purposes
+const products = []
+products.push(Schema.Product({title:'Game', id:'1', components:[]}))
+products.push(Schema.Product({title:'Webapp', id:'2', components:[]}))
+localStorage.setItem('products', JSON.stringify(products))
+
+const getProducts = () => {
+    return JSON.parse(localStorage.getItem('products'))
+}
+
 const createProduct = (product) => {
     const products = JSON.parse(localStorage.getItem('products')) || []
     products.push(product)
     localStorage.setItem('products', JSON.stringify(products))
 }
 
+export default {createProduct, getProducts}
